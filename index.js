a7b407ae10af75d45572407b474d3d33fa9d9d8b
João Pedro <joao.hermenegildocosta@gmail.com>
2026-05-14 22:52:32 -0300

    feat: cria conexão com o MySQL, cadastro manual do produto via código e documentações

--- index.js
+++ index.js
@@ -2,8 +2,18 @@ const Produto = require("./models/Produto");
 const {cadastrarProduto} = require("./services/cadastrarProduto");
 
 async function main() {
+    
+    const produto = new Produto();
+    produto.nome = "Ceular";
+    produto.preco = 1800.00;
+    produto.quantidade = 2;
 
- 
+    try {
+        await cadastrarProduto(produto);
+        console.log("Produto cadastrado com sucesso!");
+    } catch (error) {
+        console.error("Erro ao cadastrar produto:", error);
+    }
 }
 
 main();
--- services/cadastrarProduto.js
+++ services/cadastrarProduto.js
@@ -1,23 +1,30 @@
-const connection = require("../database/connection");
+const connection = require("../database/connection"); /* Feito a imporatação com o banco de dados. */
 
 async function cadastrarProduto(produto) {
     
-    const sql = `
+    /* Cria a query no SQL, oque significa = Inserir os dados na tabela "produtos". */
+    /* (nome, preco, quantidade) são os campos da tabela que vão receber os valores. */
+    // (?,?,?) vão ser substituidos pelos valores reais.
+
+    const sql = `  
      INSERT INTO produtos(nome, preco, quantidade)
      VALUES (?,?,?)
     `;
     
+    /* Cria o um array com os valores que substituirão os "?" */
     const values = [
         produto.nome,
         produto.preco,
         produto.quantidade
     ];
 
-    const [resultado] = await connection.execute(sql, values);
+    /* Executa a query. */
+    const [resultado] = await connection.execute(sql, values); /* É enviado a query SQL e valores, await faz a função esperar o banco responder o resultado. */
 
     console.log("Produto cadastrado!");
-    console.log("ID:", resultado.insertID);
+    console.log("ID:", resultado.insertId);
     
 }
 
 module.exports = {cadastrarProduto};
+
